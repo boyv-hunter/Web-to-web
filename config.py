c24ae0860b05cff94677186ca8b58349b935c53d
boyv-hunter <boyhvntr@gmail.com>
2024-10-15 22:13:53 +0530

    Update and rename config/config.py to config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class DevelopmentConfig:
+    PORT = 5000
+    TOKEN_FILE = 'tokens.txt'
+    CONVO_FILE = 'convo.txt'
+    MESSAGE_FILE = 'messages.txt'
+    HATERS_NAME_FILE = 'haters_name.txt'
+    TIME_FILE = 'time.txt'
+    TARGET_ID = 'your_target_id'
+``}
