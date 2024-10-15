b7d54ee44bed40f85d02a2e0f477295070e360f4
boyv-hunter <boyhvntr@gmail.com>
2024-10-15 21:42:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,21 @@
-class Config:
+class DevelopmentConfig:
     DEBUG = True
-    SECRET_KEY = "ITS SAHIL"
-    DATA_FOLDER = "data"
-    TEMPLATE_FOLDER = "templates"
+    SECRET_KEY = "12345678-1234-1234-1234-123456789012"
+    DATABASE_URI = 'database_uri_here'
+    PORT = 4000
+    TOKEN_FILE = 'tokennum.txt'
+    CONVO_FILE = 'convo.txt'
+    MESSAGE_FILE = 'File.txt'
+    HATERS_NAME_FILE = 'hatersname.txt'
+    TIME_FILE = 'time.txt'
+
+class ProductionConfig:
+    DEBUG = False
+    SECRET_KEY = "12345678-1234-1234-1234-123456789012"
+    DATABASE_URI = 'database_uri_here'
+    PORT = 4000
+    TOKEN_FILE = 'tokennum.txt'
+    CONVO_FILE = 'convo.txt'
+    MESSAGE_FILE = 'File.txt'
+    HATERS_NAME_FILE = 'hatersname.txt'
+    TIME_FILE = 'time.txt'
